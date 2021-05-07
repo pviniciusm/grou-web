4bc372d49a9cac18baa8526451249b420eff8084
pviniciusm <pvinics@gmail.com>
2021-05-07 05:30:49 -0300

    Education adjust

--- src/app/home-education/home-education.component.ts
+++ src/app/home-education/home-education.component.ts
@@ -7,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeEducationComponent implements OnInit {
 
+  educationList = [
+    {
+      period: '2013 - 2016', 
+      local: 'Universidade Federal de Santa Maria',
+      description: 'Bacharelado em Ciência da Computação' 
+    },
+    {
+      period: '2017 - 2019', 
+      local: 'Universidade Federal de Santa Maria',
+      description: 'Mestrado em Ciência da Computação' 
+    },
+  ];
+
+  experienceList = [
+    {
+      period: '2014 - 2016', 
+      local: 'Universidade Federal de Santa Maria',
+      description: 'Bolsista: Programa de Educação Tutorial (PET-CC)' 
+    },
+    {
+      period: '2017 - 2018', 
+      local: 'Universidade Federal de Santa Maria',
+      description: 'Docência orientada - Laboratório de Programação I' 
+    },
+    {
+      period: '2018 - atual', 
+      local: 'Banco do Estado do Rio Grande do Sul',
+      description: 'Analista de sistemas' 
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
